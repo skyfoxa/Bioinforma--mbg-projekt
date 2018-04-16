c013aaa02d0bca5f5cbeea86b3961895acbcc4fc
Marek Zvara <marek724@gmail.com>
2018-04-16 22:42:51 +0200

    fetch 1 sequence data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+
+import pysam as ps
+
+# Example to download particular data
+# ./samtools view -b ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00096/alignment/HG00096.mapped.ILLUMINA.bwa.GBR.low_coverage.20120522.bam 4:99792835-99851788 > HG00096.bam
+
+
+source = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00096/alignment/HG00096.mapped.ILLUMINA.bwa.GBR.low_coverage.20120522.bam"
+samfile = ps.AlignmentFile(source, "rb")
+
+for read in samfile.fetch('4', 99792835, 99851788):
+     print(read)
+
+samfile.close()
