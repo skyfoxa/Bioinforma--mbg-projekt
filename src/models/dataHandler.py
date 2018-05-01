a4ce18f9cb8594abada2e1a0641112619d75f543
Marek Zvara <marek724@gmail.com>
2018-05-01 20:15:23 +0200

    add populations to data info

--- src/models/dataHandler.py
+++ src/models/dataHandler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import urllib.request
+import urllib.error
 
 __authors__ = "Marek Zvara, Marek Hrvol, Filip Šamánek"
 __copyright__ = "Copyright 2018, Marek Zvara, Marek Hrvol, Filip Šamánek"
@@ -17,6 +19,7 @@ class DataHandler(object):
 
         self.__createArraysFrom__(gene1, gene2)
         self.checkIfSameGenes()
+        self.__addPopulationsToData__()
 
 
     def __createArraysFrom__(self, gene1Path, gene2Path):
@@ -38,3 +41,47 @@ class DataHandler(object):
 
     def checkIfSameGenes(self):
         assert (self.gene1Info[:,1] == self.gene2Info[:,1]).all(), "Names of the individuals do not match!"
+
+    def __addPopulationsToData__(self):
+        self.fetchPopulations()
+        
+        if len(self.populations.keys()) != 0:
+            self.gene1Info = np.apply_along_axis(self.__addPopulation__, 1, self.gene1Info)
+            self.gene2Info = np.apply_along_axis(self.__addPopulation__, 1, self.gene2Info)
+
+    def __addPopulation__(self, row):
+        population = self.populations[row[1]]
+        return np.append(row, [population])
+
+    # Returns dict - Sample_Name : Population
+    def fetchPopulations(self):
+        urlPopulations = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/20130502.phase3.analysis.sequence.index"
+        req = urllib.request.Request(urlPopulations)
+        try:
+            with urllib.request.urlopen(req) as response:
+
+                isHeader = True
+
+                self.populations = {}
+                nameIndex = -1
+                popIndex = -1
+
+                for line in response.readlines():
+                    data = line.decode('UTF-8').strip().split('\t')
+                    if isHeader:
+                        items = list(map(lambda item: item.lower(), data))
+                        popIndex = items.index('population')
+                        nameIndex = items.index('sample_name')
+                        isHeader = False
+                    else:
+                        self.populations[data[nameIndex]] = data[popIndex]
+
+                return self.populations
+        except urllib.error.HTTPError as e:
+            print(e.reason)
+        except urllib.error.URLError as e:
+            print(e.reason)
+        except:
+            print("Fetch populations: Unexpected error:")
+
+        return None
