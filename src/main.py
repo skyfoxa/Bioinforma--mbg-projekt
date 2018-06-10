a954a477f02d590985390ec818fe5ac10be19fff
Marek Zvara <marek724@gmail.com>
2018-06-10 23:30:41 +0200

    observed values for Chi squared values

--- src/main.py
+++ src/main.py
@@ -42,7 +42,9 @@ def parseArgv():
     return args
 
 def tests():
-    ChiSquared(None, None).test()
+    ChiSquared(None, None).testExpected()
+    ChiSquared(None, None).testObserved()
+
 
 def main():
     argv = parseArgv()
@@ -55,4 +57,4 @@ def main():
 
 if __name__ == "__main__":
     tests()
-    main()
+    # main()
