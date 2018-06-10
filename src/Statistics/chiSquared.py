1f9db7c120a1ee4261c8ca2baa3bcfe7be2baab5
Marek Zvara <marek724@gmail.com>
2018-06-10 21:28:20 +0200

    iStatistics - init changed - geneMatrix1, geneMatrix2

--- src/Statistics/chiSquared.py
+++ src/Statistics/chiSquared.py
@@ -8,11 +8,14 @@ __email__ = "zvaramar@fel.cvut.cz, hrvolmar@fel.cvut.cz, samanfil@fel.cvut.cz"
 __description__ = "MBG"
 
 class ChiSquared(iStatistics):
-    def __init__(self, data):
-        super().__init__(data)
+    def __init__(self, geneMatrix1, geneMatrix2):
+        super().__init__(geneMatrix1, geneMatrix2)
 
     def compute(self):
         raise Exception("iStatistics - compute(self) not implemented")
+
+    def computeCol(self):
+        ...
 
     def validate(self):
         raise Exception("iStatistics - validate(self) not implemented")
