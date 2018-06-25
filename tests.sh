c29d1eada0a75ad2be28722e79688861c17b0825
Marek Zvara <marek724@gmail.com>
2018-06-25 19:47:31 +0200

    renaming data from p.ped to original names

--- tests.sh
+++ tests.sh
@@ -11,7 +11,7 @@ echo "Test 1 - eIF4E1 vs eIF4G1, Negative checks with COX6B2, GOLGA5, POLG"
 echo "--------------------------------------------------------------------"
 
 echo "Test 1.1 (Normal) - eIF4E1 and eIF4G1"
-python3 $MAIN -gene1 "$DATA/eIF4E1/eIF4E1.ped" -gene2 "$DATA/eIF4G1/p.ped"
+python3 $MAIN -gene1 "$DATA/eIF4E1/eIF4E1.ped" -gene2 "$DATA/eIF4G1/eIF4G1.ped"
 echo ""
 
 echo "Test 1.2 (Negative check) - COX6B2 and eIF4E1"
@@ -19,7 +19,7 @@ python3 $MAIN -gene1 "$NEGATIVE/COX6B2/COX6B2.ped"  -gene2 "$DATA/eIF4E1/eIF4E1.
 echo ""
 
 echo "Test 1.3 (Negative check) - COX6B2 and elF4G1"
-python3 $MAIN -gene1 "$NEGATIVE/COX6B2/COX6B2.ped" -gene2 "$DATA/eIF4G1/p.ped"
+python3 $MAIN -gene1 "$NEGATIVE/COX6B2/COX6B2.ped" -gene2 "$DATA/eIF4G1/eIF4G1.ped"
 echo ""
 
 echo "Test 1.4 (Negative check) - GOLGA5 and eIF4E1"
@@ -27,7 +27,7 @@ python3 $MAIN -gene1 "$NEGATIVE/GOLGA5/GOLGA5.ped" -gene2 "$DATA/eIF4E1/eIF4E1.p
 echo ""
 
 echo "Test 1.5 (Negative check) - GOLGA5 and elF4G1"
-python3 $MAIN -gene1 "$NEGATIVE/GOLGA5/GOLGA5.ped" -gene2 "$DATA/eIF4G1/p.ped"
+python3 $MAIN -gene1 "$NEGATIVE/GOLGA5/GOLGA5.ped" -gene2 "$DATA/eIF4G1/eIF4G1.ped"
 echo ""
 
 echo "Test 1.6 (Negative check) - POLG and eIF4E1"
@@ -35,5 +35,5 @@ python3 $MAIN -gene1 "$NEGATIVE/POLG/POLG.ped" -gene2 "$DATA/eIF4E1/eIF4E1.ped"
 echo ""
 
 echo "Test 1.7 (Negative check) - POLG and elF4G1"
-python3 $MAIN -gene1 "$NEGATIVE/POLG/POLG.ped" -gene2 "$DATA/eIF4G1/p.ped"
+python3 $MAIN -gene1 "$NEGATIVE/POLG/POLG.ped" -gene2 "$DATA/eIF4G1/eIF4G1.ped"
 echo ""
