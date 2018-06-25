2f50e030423ed42a2feb20b18bbc0e2d12e44ac8
Marek Zvara <marek724@gmail.com>
2018-06-25 19:06:57 +0200

    permutation test - implemented interface for getting results

--- src/Statistics/permutationTest.py
+++ src/Statistics/permutationTest.py
@@ -13,36 +13,26 @@ __email__ = "zvaramar@fel.cvut.cz, hrvolmar@fel.cvut.cz, samanfil@fel.cvut.cz"
 __description__ = "MBG"
 
 class PermutationTest(iStatistics):
-    plotTitle = None
+    testResults = {}
 
-    def __init__(self, geneMatrix1, geneMatrix2, testClass, plotTitle="PermutationTest"):
+    def __init__(self, geneMatrix1, geneMatrix2, testClass):
         super().__init__(geneMatrix1, geneMatrix2)
         self.test = globals()[testClass]
 
         if not issubclass(self.test, iStatistics):
             raise AttributeError("testClass must be subclass of iStatistics")
 
-        self.plotTitle = plotTitle
 
     def __permutate__(self, geneMatrix):
         return np.random.permutation(geneMatrix)
 
     def compute(self):
-        test = self.test(self.__permutate__(self.geneMatrix1), self.geneMatrix2, plotTitle=self.plotTitle)
+        test = self.test(self.__permutate__(self.geneMatrix1), self.geneMatrix2)
         test.compute()
+        self.testResults = test.getResults()
 
+    def getResults(self):
+        return self.testResults
 
     def validate(self):
         raise Exception("iStatistics - validate(self) not implemented")
-
-    def plot(self, values, title):
-        ...
-        # plt.plot(values)
-        # plt.title(title)
-        # plt.savefig(title)
-        # plt.close()
-        #
-        # num_bins = 5
-        # n, bins, patches = plt.hist(values, num_bins, facecolor='blue', alpha=0.5)
-        # plt.savefig("Hist" + title)
-        # plt.close()
