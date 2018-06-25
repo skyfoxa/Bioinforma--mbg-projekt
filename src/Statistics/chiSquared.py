0f2e3d691edaebc762db1f4ec76276c17374c79b
Marek Zvara <marek724@gmail.com>
2018-06-25 19:06:10 +0200

    chiSquared - removed plot method + interface method for results

--- src/Statistics/chiSquared.py
+++ src/Statistics/chiSquared.py
@@ -3,8 +3,6 @@
 from .iStatistics import iStatistics
 from src.Statistics.statisticsUtilities import *
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 from src.Configuration.applicationConfig import Config
 
 __authors__ = "Marek Zvara, Marek Hrvol, Filip Šamánek"
@@ -13,27 +11,22 @@ __email__ = "zvaramar@fel.cvut.cz, hrvolmar@fel.cvut.cz, samanfil@fel.cvut.cz"
 __description__ = "MBG"
 
 class ChiSquared(iStatistics):
-    plotTitle = None
     c1 = 0
     c2 = 0
+    vals = []
 
-    def __init__(self, geneMatrix1, geneMatrix2, plotTitle = "ChiSquared"):
+    def __init__(self, geneMatrix1, geneMatrix2):
         super().__init__(geneMatrix1, geneMatrix2)
-        self.plotTitle = plotTitle
 
     def compute(self):
-        vals = []
+        self.vals = []
         for colM1 in self.geneMatrix1.T:
             for colM2 in self.geneMatrix2.T:
                 chiSquared = self.__computeCols__(colM1, colM2)
-                vals.append(chiSquared)
+                self.vals.append(chiSquared)
 
-        Config.setChiSquaredThreshold(chiSquaredValues=vals)
-        self.classifySamples(vals)
-
-        self.printResult()
-        self.plot(vals, self.plotTitle)
-
+        Config.setChiSquaredThreshold(chiSquaredValues=self.vals)
+        self.__classifySamples__(self.vals)
 
 
     # returns chiSquared value
@@ -43,12 +36,15 @@ class ChiSquared(iStatistics):
 
         return np.sum(np.divide(np.square(np.subtract(observed, expected)), expected))
 
-    def classifySamples(self, samples):
+    def __classifySamples__(self, samples):
         for sample in samples:
             if sample > Config.CHI_SQUARED_THRESHOLD:
                 self.c1 += 1
             else:
                 self.c2 += 1
+
+    def getResults(self):
+        return {"values" : self.vals, "positive" : self.c1, "negative" : self.c2, "ratio" : float(self.c1) / float(self.c2) * 100}
 
     def printResult(self):
         print("Positive: " + str(self.c1))
@@ -58,30 +54,7 @@ class ChiSquared(iStatistics):
     def validate(self):
         raise Exception("iStatistics - validate(self) not implemented")
 
-    def plot(self, values, title):
-        plt.plot(values)
-        plt.title(title)
-        plt.show()
-        # plt.savefig(title)
-        plt.close()
-
-        #norm.rvs(10.0, 2.5, size=500)
-        # Fit a normal distribution to the data:
-        mu, std = norm.fit(values)
-
-        # Plot the histogram.
-        plt.hist(values, bins=50, alpha=0.6, color='g')
-
-        # Plot the PDF.
-        xmin, xmax = plt.xlim()
-        x = np.linspace(xmin, xmax, 100)
-        p = norm.pdf(x, mu, std)
-        plt.plot(x, p, 'k', linewidth=2)
-        # title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
-        plt.title("Hist " + title)
-        # plt.savefig("Hist" + title)
-        plt.show()
-        plt.close()
+    ## Tests
 
     def testExpected(self):
         col1 = np.array([1, 0, 1, 0, 0])
