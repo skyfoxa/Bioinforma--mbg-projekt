83c2bc430f19fc112dadce4c495844e03aa333af
Marek Zvara <marek724@gmail.com>
2018-06-25 15:09:02 +0200

    chi squared treshold config computation

--- src/Configuration/applicationConfig.py
+++ src/Configuration/applicationConfig.py
@@ -4,16 +4,17 @@ class Config:
     SET_CHI_SQUARED_DYNAMICALLY = True
     CHI_SQUARED_EPSILON = 4
 
-
-    def setChiSquaredThreshold(self, chiSquaredValues):
-        if not self.SET_CHI_SQUARED_DYNAMICALLY:
+    @staticmethod
+    def setChiSquaredThreshold(chiSquaredValues):
+        if not Config.SET_CHI_SQUARED_DYNAMICALLY:
             return
-        self.SET_CHI_SQUARED_DYNAMICALLY = False
-        self.CHI_SQUARED_THRESHOLD = 10.828 # TODO: logic for setting chi squared
-        chiSquaredValues = chiSquaredValues.sort()
-        for i in range (len(chiSquaredValues)-5):
-            if chiSquaredValues[i+5] - chiSquaredValues[i] > self.CHI_SQUARED_EPSILON:
-                print(chiSquaredValues[i])
+        Config.SET_CHI_SQUARED_DYNAMICALLY = False
+        Config.CHI_SQUARED_THRESHOLD = 10.828 # TODO: logic for setting chi squared
+        sortedVals = sorted(chiSquaredValues)
+        for i in range (len(sortedVals)-5):
+            if sortedVals[i+5] - sortedVals[i] > Config.CHI_SQUARED_EPSILON:
+                print(sortedVals[i])
+                Config.CHI_SQUARED_THRESHOLD = sortedVals[i]
                 return
 
 
--- src/Statistics/chiSquared.py
+++ src/Statistics/chiSquared.py
@@ -16,8 +16,6 @@ __email__ = "zvaramar@fel.cvut.cz, hrvolmar@fel.cvut.cz, samanfil@fel.cvut.cz"
 __description__ = "MBG"
 
 class ChiSquared(iStatistics):
-
-    threshold01 = None
     plotTitle = None
     c1 = 0
     c2 = 0
@@ -30,31 +28,33 @@ class ChiSquared(iStatistics):
         vals = []
         for colM1 in self.geneMatrix1.T:
             for colM2 in self.geneMatrix2.T:
-                pVal = self.__computeCols__(colM1, colM2)
-                if pVal is not None:
-                    vals.append(pVal)
+                chiSquared = self.__computeCols__(colM1, colM2)
+                vals.append(chiSquared)
+
+        Config.setChiSquaredThreshold(chiSquaredValues=vals)
+        vals = list(filter(lambda val: val >= Config.CHI_SQUARED_THRESHOLD, vals))
 
         print("Positive: " + str(self.c1))
         print("Negative: " + str(self.c2))
-        print("Ratio: " + str(float(self.c1) / float(self.c2) * 100) + " %")
+        # print("Ratio: " + str(float(self.c1) / float(self.c2) * 100) + " %")
         self.plot(vals, self.plotTitle)
 
 
 
-
+    # returns chiSquared value
     def __computeCols__(self, colM1, colM2):
         expected = StatisticsUtilities.calculateExpected(colM1, colM2)
         observed = StatisticsUtilities.calculateObserved(colM1, colM2)
 
-        pVal = np.sum(np.divide(np.square(np.subtract(observed, expected)), expected))
+        return np.sum(np.divide(np.square(np.subtract(observed, expected)), expected))
 
-        if pVal > Config.CHI_SQUARED_THRESHOLD:
-            # print(pVal)
-            self.c1 += 1
-            return pVal
-        else:
-            self.c2 += 1
-            return None
+        # if chiSquared > Config.CHI_SQUARED_THRESHOLD:
+        #     # print(pVal)
+        #     self.c1 += 1
+        #     return chiSquared
+        # else:
+        #     self.c2 += 1
+        #     return None
 
 
     def validate(self):
